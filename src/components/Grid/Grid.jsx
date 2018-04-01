a7228de8a1b81455284a09f83d05df46af3aa9f5
Jonathan Delefortrie <jonathandelefortrie@gmail.com>
2018-04-01 20:47:11 +1000

    grid classname

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -3,9 +3,17 @@ import PropTypes from 'prop-types';
 
 class Grid extends Component {
   render() {
-    const { children } = this.props;
-    return <div className="mdl-grid">{children}</div>;
+    const { children, className } = this.props;
+    return <div className={`mdl-grid ${className}`}>{children}</div>;
   }
 }
+
+Grid.propTypes = {
+  className: PropTypes.string
+};
+
+Grid.defaultProps = {
+  className: ''
+};
 
 export default Grid;
