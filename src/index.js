39ae4fe107816ff4a1f7daf93ac7207e84be264f
Jonathan Delefortrie <jonathandelefortrie@gmail.com>
2018-04-06 06:30:21 +1000

    compressed css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if (typeof window !== 'undefined' && !!window.document) {
-  require('material-design-lite/material.min.js');
+  require('material-design-lite/material.js');
 }
 
 import components from './components';
